1541cbcb5902bc1f9e88517ef03f0f7d56007b26
Plantagenet <plantagenet@Novascholastica.local>
2013-03-21 12:44:51 +0100

    Initial Commit

---
+++ PaperCitekeyHandler/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PaperCitekeyHandler
+//
+//  Created by Plantagenet on 21/03/13.
+//  Copyright (c) 2013 Plantagenet. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ PaperCitekeyHandler/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PaperCitekeyHandler
+//
+//  Created by Plantagenet on 21/03/13.
+//  Copyright (c) 2013 Plantagenet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
